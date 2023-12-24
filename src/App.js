677fbbf29e6b3070d22cb3f31d0087a72f83a889
Hrutwik Dabhade <dabhadehrutwik@gmail.com>
2023-12-24 19:05:40 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Header from './components/Header';
+import AppointmentForm from './components/AppointmentForm';
+import PatientList from './components/PatientList';
+import Hospital from './components/Hospital';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+        <Route path='Hospital' element={<Hospital></Hospital>}></Route>
+          <Route path='AppointmentForm' element={<AppointmentForm></AppointmentForm>}></Route>
+          <Route path='PatientList' element={<PatientList></PatientList>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
